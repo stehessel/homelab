2ea47c651d50e054c8cf0dfc377f69cc8160439b
stehessel <stephan@stehessel.de>
2022-11-12 23:45:01 +0100

    add versions

--- terraform/hetzner/main.tf
+++ terraform/hetzner/main.tf
@@ -63,25 +63,3 @@ module "kube-hetzner" {
 provider "hcloud" {
   token = data.sops_file.secrets.data["hcloud.token"]
 }
-
-terraform {
-  required_version = ">= 1.2.0"
-  required_providers {
-    hcloud = {
-      source  = "hetznercloud/hcloud"
-      version = ">= 1.35.1"
-    }
-    sops = {
-      source  = "carlpett/sops"
-      version = "~> 0.5"
-    }
-  }
-  backend "s3" {
-    bucket                      = "terraform-backend"
-    endpoint                    = "s3.us-west-000.backblazeb2.com"
-    key                         = "terraform.tfstate"
-    region                      = "us-east-1" # Meaningless, but the provider needs it. It can be any string.
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-  }
-}
--- terraform/hetzner/outputs.tf
+++ terraform/hetzner/outputs.tf
@@ -1,15 +1,15 @@
 output "kubeconfig" {
-  value       = module.kube-hetzner.kubeconfig_file
-  sensitive   = true
   description = "The kubeconfig file to access the cluster via kubectl."
+  sensitive   = true
+  value       = module.kube-hetzner.kubeconfig_file
 }
 
 output "control_planes_public_ipv4" {
-  value       = module.kube-hetzner.control_planes_public_ipv4
   description = "The IPv4 addresses of all control plane nodes."
+  value       = module.kube-hetzner.control_planes_public_ipv4
 }
 
 output "agents_public_ipv4" {
-  value       = module.kube-hetzner.agents_public_ipv4
   description = "The IPv4 addresses of all agent nodes."
+  value       = module.kube-hetzner.agents_public_ipv4
 }
