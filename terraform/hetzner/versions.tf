c310c9e9fa62bd6bdbed10cead5c2e99542b82cc
stehessel <stephan@stehessel.de>
2022-11-13 04:16:41 +0100

    remove fluxcd

--- terraform/hetzner/versions.tf
+++ terraform/hetzner/versions.tf
@@ -10,28 +10,6 @@ terraform {
       source  = "carlpett/sops"
       version = "~> 0.5"
     }
-
-    # Fluxcd
-    github = {
-      source  = "integrations/github"
-      version = ">= 4.5.2"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.2"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.10.0"
-    }
-    flux = {
-      source  = "fluxcd/flux"
-      version = ">= 0.0.13"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "3.1.0"
-    }
   }
   backend "s3" {
     bucket                      = "terraform-backend"
