810a224acbf2adb5b0f18554a3ad036a63da3b00
stehessel <stephan@stehessel.de>
2022-11-19 04:38:03 +0100

    add pre-commit flake

---
+++ flake.nix
@@ -0,0 +1,90 @@
+{
+  description = "stehessel's homelab";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    pre-commit-hooks,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+
+            hooks = {
+              # Nix
+              alejandra.enable = true;
+              deadnix.enable = true;
+              statix.enable = true;
+
+              # YAML
+              yamllint = {
+                enable = true;
+                entry = pkgs.lib.mkForce "${pkgs.yamllint}/bin/yamllint --config-file .github/lint/.yamllint.yaml";
+              };
+
+              # Pre-commit
+              check-merge-conflict = {
+                enable = true;
+                name = "check for merge conflicts";
+                description = "checks for files that contain merge conflict strings.";
+                entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/check-merge-conflict";
+                language = "python";
+                types = ["text"];
+              };
+              detect-private-key = {
+                enable = true;
+                name = "detect private key";
+                description = "detects the presence of private keys.";
+                entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/detect-private-key";
+                language = "python";
+                types = ["text"];
+              };
+              end-of-file-fixer = {
+                enable = true;
+                name = "fix end of files";
+                description = "ensures that a file is either empty, or ends with one newline.";
+                entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/end-of-file-fixer";
+                language = "python";
+                types = ["text"];
+                stages = ["commit" "push" "manual"];
+              };
+              trailing-whitespace = {
+                enable = true;
+                name = "trim trailing whitespace";
+                description = "trims trailing whitespace.";
+                entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/trailing-whitespace-fixer";
+                language = "python";
+                types = ["text"];
+                stages = ["commit" "push" "manual"];
+              };
+            };
+
+            settings = {
+              statix.ignore = [".direnv"];
+            };
+          };
+        };
+        devShells.default = nixpkgs.legacyPackages.${system}.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+        };
+      }
+    );
+}
