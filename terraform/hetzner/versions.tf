887180398f42d1a0d1bbaad0b5254086f64807a4
stehessel <stephan@stehessel.de>
2022-11-27 14:08:11 +0100

    enable traefik gateway

--- terraform/hetzner/versions.tf
+++ terraform/hetzner/versions.tf
@@ -15,12 +15,12 @@ terraform {
       version = "~> 0.5"
     }
   }
-  backend "s3" {
-    bucket                      = "terraform-backend"
-    endpoint                    = "s3.us-west-000.backblazeb2.com"
-    key                         = "terraform.tfstate"
-    region                      = "us-east-1" # Meaningless, but the provider needs it. It can be any string.
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-  }
+  # backend "s3" {
+  #   bucket                      = "terraform-backend"
+  #   endpoint                    = "s3.us-west-000.backblazeb2.com"
+  #   key                         = "terraform.tfstate"
+  #   region                      = "us-east-1" # Meaningless, but the provider needs it. It can be any string.
+  #   skip_credentials_validation = true
+  #   skip_metadata_api_check     = true
+  # }
 }
